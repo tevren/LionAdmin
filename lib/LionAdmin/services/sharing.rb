42e5237316dcfea2a64af2eafd3b290d5c141b65
Anurag Mohanty <anurag@columbia.edu>
2012-09-18 15:39:40 -0400

    added get/set State, read/write Settings, getLogPaths, tailFile and a fix in the dhcp classname from afp to dhcp

--- lib/LionAdmin/services/sharing.rb
+++ lib/LionAdmin/services/sharing.rb
@@ -1,7 +1,23 @@
 require 'plist'
 module LionAdmin
 	class Sharing < Service
-		def tailFile
+		
+		def tailFile(params={:id => "",:path => "",:offset => 0,:amount => 10000},options={})
+			if params[:id].blank?
+				cmd = "#{service_name}:command = tailFile\n#{service_name}:path = #{params[:path]}\n#{service_name}:offset = #{params[:offset]}\n#{service_name}:amount = #{params[:amount]}"
+			else
+				cmd = "#{service_name}:command = tailFile\n#{service_name}:identifier = #{params[:id]}\n#{service_name}:offset = #{params[:offset]}\n#{service_name}:amount = #{params[:amount]}"
+			end
+			tmp_command_file = "/tmp/#{service_name}.command.#{Time.current.to_formatted_s(:number)}"
+			File.open(tmp_command_file, 'w') {|f| 
+				f.write(cmd)
+				f.close
+			}
+			plist = %x[#{@user_prefix} sudo #{@serveradmin} -x command < #{tmp_command_file}]
+			tailFile = Plist::parse_xml(plist)
+			File.delete(tmp_command_file)
+			return tailFile
 		end
+
 	end
 end
